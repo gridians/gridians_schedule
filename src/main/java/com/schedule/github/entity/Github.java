933cb09058cc2364a8d29ae3e21bc7d5b04bcaae
lightbell03 <bellight28@gmail.com>
2023-01-14 13:38:17 +0900

    #1 feature: entity init

---
+++ src/main/java/com/schedule/github/entity/Github.java
@@ -0,0 +1,51 @@
+package com.schedule.github.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Github {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+
+    private String url;
+
+    @Column(name = "followers_url")
+    private String followersUrl;
+
+    @Column(name = "following_url")
+    private String followingUrl;
+
+    private String location;
+
+    @Column(name = "created_at")
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "modified_at")
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+}
